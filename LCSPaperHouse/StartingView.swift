16a38b870755963548fdbdb2ffda5cfe8e8748c5
Dawn Liu <dliu@lcs.on.ca>
2024-05-31 15:11:29 -0400

    created the StartingView(first view) of LCSPaperHouse APP

---
+++ LCSPaperHouse/StartingView.swift
@@ -0,0 +1,23 @@
+//
+//  StartingView.swift
+//  LCSPaperHouse
+//
+//  Created by Chenxi Liu on 2024/5/31.
+//
+
+import SwiftUI
+
+struct StartingView: View {
+    var body: some View {
+        VStack {
+            Image("LCS logo")
+            Text("Paper house")
+            Text("Get started")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    StartingView()
+}
