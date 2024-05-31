da64396ef7b80e55d68e9f16dcc5ab6080d94081
Dawn Liu <dliu@lcs.on.ca>
2024-05-31 15:06:29 -0400

    Initial Commit

---
+++ LCSPaperHouse/LCSPaperHouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LCSPaperHouseApp.swift
+//  LCSPaperHouse
+//
+//  Created by Chenxi Liu on 2024/5/31.
+//
+
+import SwiftUI
+
+@main
+struct LCSPaperHouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
